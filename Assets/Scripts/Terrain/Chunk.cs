d3c89c867df88f7883961fc08660acf91c45a7a7
Norflip <Norgren.filip@gmail.com>
2022-02-05 14:27:25 +0100

    working terrain

--- Assets/Scripts/Terrain/Chunk.cs
+++ Assets/Scripts/Terrain/Chunk.cs
@@ -1,17 +1,66 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Jobs;
+using Unity.Collections;
+using Unity.Burst;
+using Unity.Jobs;
+using Unity.Mathematics;
+
+[BurstCompile]
+public struct MeshHeightJob : IJobParallelFor
+{
+    public NativeArray<Vector3> vertices;
+
+    public float scale; // 1
+    public float height; // 3
+    public float positionZOffset;
+
+    public void Execute(int index)
+    {
+        Vector3 vert = vertices[index];
+        float2 p = math.float2(vert.x * scale, (vert.z + positionZOffset) * scale);
+        vertices[index] = new Vector3(vert.x, noise.snoise(p), vert.z);
+    }
+}
 
 public class Chunk : MonoBehaviour
 {
     public const int Size = 32;
-    // generate terrain
-    int index;
+    NativeArray<Vector3> vertices;
+
+    [SerializeField] int index;
+    Mesh mesh;
     
-    public void Generate (int index)
+    private void OnValidate() {
+        transform.position = new Vector3(0.0f, 0.0f, index * Size);
+    }
+
+    void Awake () {
+        mesh = GetComponent<MeshFilter>().mesh; 
+        vertices = new NativeArray<Vector3>(mesh.vertices, Allocator.Persistent); // 2
+    }
+
+    public void Generate (int index, float noiseScale, float height)
     {
         this.index = index;
         transform.position = new Vector3(0.0f, 0.0f, index * Size);
+
+        MeshHeightJob job = new MeshHeightJob();
+        job.scale = noiseScale;
+        job.height = height;
+        job.vertices = vertices;
+        job.positionZOffset = index * Size;
+        job.Schedule(vertices.Length, 64).Complete();
+
+        mesh.SetVertices(job.vertices);
+        mesh.RecalculateNormals();
+        mesh.RecalculateBounds();
+    }
+
+    private void OnDestroy()
+    {
+        vertices.Dispose();
     }
 
     private void OnDrawGizmos() {
--- Assets/Scripts/Terrain/ChunkManager.cs
+++ Assets/Scripts/Terrain/ChunkManager.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class ChunkManager : MonoBehaviour
 {
     public Transform player;
+    public Chunk chunk0;
+    public Chunk chunk1;
+
+    public float noiseScale = 0.1f;
+    public float noiseHeight = 2.0f;
 
     Chunk[] chunks;
     int currentWorldIndex;
 
-
-    int GetPlayerOnChunkIndex ()
-    {
-        return Mathf.FloorToInt(player.position.z / Chunk.Size);
+    private void Awake() {
+        currentWorldIndex = GetPlayerOnChunkIndex();
+        chunks = new Chunk[2] {chunk0, chunk1};
+        chunk0.Generate(0, noiseScale, noiseHeight);
+        chunk1.Generate(1, noiseScale, noiseHeight);
     }
 
     void Update ()
@@ -20,7 +26,13 @@ public class ChunkManager : MonoBehaviour
         int index = GetPlayerOnChunkIndex();
         if(index != currentWorldIndex)
         {
-
+            int activeChunk = index % 2;
+            
         }
+    }
+
+    int GetPlayerOnChunkIndex ()
+    {
+        return Mathf.FloorToInt(player.position.z / Chunk.Size);
     }
 }
