3fc015c1664a0923603e3f58b1b495aefcb45993
Norflip <Norgren.filip@gmail.com>
2022-02-06 21:26:15 +0100

    awdawd

--- Assets/Scripts/ColorComp/Comp.cs
+++ Assets/Scripts/ColorComp/Comp.cs
@@ -5,8 +5,45 @@ using UnityEngine;
 
 public class Comp : MonoBehaviour
 {
+    public PaintingImage image0;
+    public PaintingImage image1;
+    
+
     public ComputeShader round_cs;
     public ComputeShader compare_cs;
+
+    private void Awake() {
+        Test(image0, image0);
+        Test(image0, image1);
+    }
+
+    public void Test (PaintingImage m0, PaintingImage m1)
+    {
+        //RenderTexture a = RoundTextureToPalette(m0);
+        //RenderTexture b = RoundTextureToPalette(m1);
+
+        int maxValue = m0.image.width * m0.image.height * 10;
+        int[] c= new int[1]{0};
+        ComputeBuffer counter = new ComputeBuffer(1, sizeof(int));
+        counter.SetData(c);
+
+        compare_cs.SetTexture(0, "_TextureA", m0.image);
+        compare_cs.SetTexture(0, "_TextureB", m1.image);
+        compare_cs.SetBuffer(0, "_Counter", counter);
+
+        int thrx = Mathf.CeilToInt(m0.image.width / 8.0f);
+        int thry = Mathf.CeilToInt(m0.image.height / 8.0f);
+        compare_cs.Dispatch(0, thrx, thry, 1);
+        counter.GetData(c);
+
+        float percentage = (float)c[0] / (m0.image.width * m0.image.height * 10.0f); //1.0f - (c[0] / 255.0f) / (m0.image.width * m0.image.height);
+
+        Debug.Log("Count: " + c[0]);
+        Debug.Log("D: " + ((m0.image.width * m0.image.height * 10.0f)));
+        Debug.Log("PERCENTAGE: " + percentage);
+
+        counter.Dispose();
+    }
 
     public float Compare (RenderTexture rt, PaintingImage image)
     {
@@ -32,11 +69,15 @@ public class Comp : MonoBehaviour
     public RenderTexture RoundTextureToPalette (PaintingImage image)
     {
         RenderTexture rt = new RenderTexture(image.image.width, image.image.height, 0);
+        rt.enableRandomWrite = true;
+        rt.filterMode = FilterMode.Point;
+        rt.Create();
+
         ComputeBuffer paletteBuffer = new ComputeBuffer(image.palette.Length, sizeof(float) * 4);
         paletteBuffer.SetData(image.palette);
 
         round_cs.SetTexture(0, "_Source", image.image);
-        round_cs.SetTexture(0, "_Target", rt);
+        round_cs.SetTexture(0, "_Result", rt);
         round_cs.SetBuffer(0, "_Palette", paletteBuffer);
 
         int thrx = Mathf.CeilToInt(image.image.width / 8.0f);
